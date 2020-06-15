fedbe6d8f544d483cc2a030fb3bdf5e855bf7ac2
Tatyana <hanyatan@gmail.com>
2020-06-15 18:59:02 -0400

    final code, added history list

--- README.md
+++ README.md
@@ -3,17 +3,18 @@
 ![weather dashboard demo](./Assets/Screen-Shot.png)
 
 
+Plan to build the Weather Dashboard:
 
+1. Get an API, Set working URL;
+2. Put Font icon of weather;
+3. Retrieve user's input and convert to variables;
+4. User variables to run AJAX call to Weather website, create function to take the user's input city and display current weather of chosen city and forecast for the next 3-5 days;
+5. Break down the Weather Object into useable fields;
+6. Dynamically generate HTML content;
+7. Dealing with "edge cases" - bugs;
+8. Attach screenshot;
+9. Create list of default cities;
+10. When user clicks the city in the history list, he gets the current forecast for that city.
 
-1. Get API, Set working URL
-2. Font icons of weather
-3. Retrieve user's input and convert to variables
-4. User variables to run AJAX call to Weather website
-5. Break down the Weather Object into useable fields
-6. Dynamically generate HTML content
-7. Dealing with "edge cases" - bugs
-8. attach screenshot
-9. create list of default cities.
-10. google autocompletion for cities.???
 
 
--- script.js
+++ script.js
@@ -1,108 +1,131 @@
-// $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-$("#currentDay").text(moment().format('LLL')); 
-
-var userInput = ""
-// var API_Key = "AIzaSyDLg0h8zDLyv1VP0PhbepucrbsQxQ1jFao";
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=" + API_Key;
-// // var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}"
+$( document ).ready(function() {
 
 
 
-// const searchElement = document.getElementById("search-value");
-// const searchBox = new google.maps.places.SearchBox(searchElement);
-// searchBox.addListener("places_changed", () => {
-//     const place = searchBox.getPlaces()[0]
-//     if (place == null) return
-//     const lat = place.geometry.location.lat();
-//     const lon = place.geometry.location.lng();
-//     fetch("/weather", {
-//         method:"POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "application/json"
-//         },
-//         body: JSON.stringify({
-//             latitude: lat,
-//             longitude: lon,
-//         })
-//     }).then(res => res.json()).then(data => {
-//         setWeatherData(data, place.formatted_address)
-//     })
-//     console.log(lon, lat);
-// })
+    // $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-const searchElement = $("#search-button");
-searchElement.click( function ajaxCall() {
 
-    userInput = $("#search-value").val();
-    var queryURL = "https://api.weatherapi.com/v1/forecast.json?key=" + window.API_Key + "&days=7" + "&q=" + userInput;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(data) {
+    $("#currentDay").text(moment().format('LLL')); 
 
-        // clear forecast body
+    var userInput = ""
+    // var API_Key = "AIzaSyDLg0h8zDLyv1VP0PhbepucrbsQxQ1jFao";
+    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=" + API_Key;
+    // // var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}"
+
+
+
+    // const searchElement = document.getElementById("search-value");
+    // const searchBox = new google.maps.places.SearchBox(searchElement);
+    // searchBox.addListener("places_changed", () => {
+    //     const place = searchBox.getPlaces()[0]
+    //     if (place == null) return
+    //     const lat = place.geometry.location.lat();
+    //     const lon = place.geometry.location.lng();
+    //     fetch("/weather", {
+    //         method:"POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Accept": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             latitude: lat,
+    //             longitude: lon,
+    //         })
+    //     }).then(res => res.json()).then(data => {
+    //         setWeatherData(data, place.formatted_address)
+    //     })
+    //     console.log(lon, lat);
+    // })
+    function clear() {
+        $("#forecast").empty();
+        $("#forecastRow").empty();
+        // $(".forecast").empty();
+        $("#fiveDayForecast").empty();
+        $("#search-value").val("");
+
+    };
+
+    function ajaxCall() {
+
+        var queryURL = "https://api.weatherapi.com/v1/forecast.json?key=" + window.API_Key + "&days=7" + "&q=" + userInput;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(data) {
+
+            // clear forecast body
+            clear();
+
+            console.log(data);
+            var location = $("#location");
+            var listHistory = $(".list-group");
+            if (location) {
+                var list = $("<li>");
+                list.addClass("list");
+                list.attr("id", data.location.name);
+                list.text(data.location.name);
+                listHistory.prepend(list);
+            }
+            // $("#p7menubar > li:nth-child(9) > .trigger").attr("id","MyNewID");
+
+
+            // putting data from AJAX to the forecast body
+            location.text(data.location.name + ", " + data.location.country);
+            var temp = $("<p>");
+            temp.attr("id", "humid");
+            temp.text("Temperature: " + data.current.temp_c + " C / " + data.current.temp_f + " F");
+            $("#forecast").append(temp);
+            var humid = $("<p>");
+            humid.attr("id", "humid");
+            humid.text("Humidity: " + data.current.humidity);
+            $("#forecast").append(humid);
+            var wind = $("<p>");
+            wind.attr("id", "wind");
+            wind.text("Wind Speed: " + data.current.wind_mph + " mph");
+            $("#forecast").append(wind);
+            var uv = $("<p>");
+            uv.attr("id", "uv");
+            uv.text("UV Index: " + data.current.uv);
+            $("#forecast").append(uv);
+
+            // 3-5 days forecast
+            var forecastRowTitle = $("<div>");
+            forecastRowTitle.addClass("col-lg-12 forecast").css({"font-size": "30px", "color": "blue", "padding": "20px"});
+            forecastRowTitle.text(data.forecast.forecastday.length + "-day Forecast:");
+
+            $("#fiveDayForecast").prepend(forecastRowTitle);
+
+
+            for ( var i = 0; i < data.forecast.forecastday.length; i ++) {
+                var forecastCol = $("<div>");
+                forecastCol.addClass("col-md-" + data.forecast.forecastday.length);
+                var forecastP = $("<p>");
+                forecastP.text("Date: " + data.forecast.forecastday[i].date)
+                var forecastTC = $("<p>");
+                forecastTC.text("Temperature: " + data.forecast.forecastday[i].day.maxtemp_c + " C / " + data.forecast.forecastday[i].day.maxtemp_f + " F");
+                var forecastHum = $("<p>");
+                forecastHum.text("Humidity: " + data.forecast.forecastday[i].day.avghumidity);
+                forecastCol.append(forecastP, forecastTC, forecastHum);
+                // $("#forecastRow").append(forecastCol);
+                $("#fiveDayForecast").append(forecastCol);
+            }
+        })
+    }
+
+    const searchElement = $("#search-button");
+    searchElement.click( function () {
+        userInput = $("#search-value").val();
+        ajaxCall();
+        
+    });
+
+
+
+    $("#cityList").on("click", ".list", function () {
+        userInput = $(this).attr("id");
+        console.log(userInput);
         clear();
-
-        console.log(data);
-        var location = $("#location");
-        var listHistory = $(".list-group");
-        if (location) {
-            var list = $("<li>");
-            list.addClass("list-group");
-            list.text(data.location.name);
-            listHistory.prepend(list);
-        }
-
-        // putting data from AJAX to the forecast body
-        location.text(data.location.name + ", " + data.location.country);
-        var temp = $("<p>");
-        temp.attr("id", "humid");
-        temp.text("Temperature: " + data.current.temp_c + " C / " + data.current.temp_f + " F");
-        $("#forecast").append(temp);
-        var humid = $("<p>");
-        humid.attr("id", "humid");
-        humid.text("Humidity: " + data.current.humidity);
-        $("#forecast").append(humid);
-        var wind = $("<p>");
-        wind.attr("id", "wind");
-        wind.text("Wind Speed: " + data.current.wind_mph + " mph");
-        $("#forecast").append(wind);
-        var uv = $("<p>");
-        uv.attr("id", "uv");
-        uv.text("UV Index: " + data.current.uv);
-        $("#forecast").append(uv);
-
-        // 3-5 days forecast
-        var forecastRowTitle = $("<div>");
-        forecastRowTitle.addClass("row forecast").css({"font-size": "30px", "color": "blue", "padding": "20px"});
-        forecastRowTitle.text(data.forecast.forecastday.length + "-day Forecast:");
-        $("#fiveDayForecast").prepend(forecastRowTitle);
-
-        for ( var i = 0; i < data.forecast.forecastday.length; i ++) {
-            var forecastCol = $("<div>");
-            forecastCol.addClass("col-md-" + data.forecast.forecastday.length);
-            var forecastP = $("<p>");
-            forecastP.text("Date: " + data.forecast.forecastday[i].date)
-            var forecastTC = $("<p>");
-            forecastTC.text("Temperature: " + data.forecast.forecastday[i].day.maxtemp_c + " C / " + data.forecast.forecastday[i].day.maxtemp_f + " F");
-            var forecastHum = $("<p>");
-            forecastHum.text("Humidity: " + data.forecast.forecastday[i].day.avghumidity);
-            forecastCol.append(forecastP, forecastTC, forecastHum);
-            $("#forecastRow").append(forecastCol);
-        }
+        ajaxCall();
+        
     })
 });
-
-function clear() {
-    $("#forecast").empty();
-    $("#forecastRow").empty();
-    $(".forecast").empty();
-
-};
-
-$(".list-group").on("click", function () {
-    // userInput = $("button").val();
-    alert("HI")
-    
-})
